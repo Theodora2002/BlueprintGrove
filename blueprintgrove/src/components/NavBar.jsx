d95b9fa72d3a7bcd061f3771049180b55f50aaee
JOMUMWE <muindijoshua032@gmail.com>
2024-05-01 14:44:05 +0300

    navbar
    alsmost done with

--- blueprintgrove/src/components/NavBar.jsx
+++ blueprintgrove/src/components/NavBar.jsx
@@ -1,7 +1,14 @@
-import { FaXTwitter, FaFacebook, FaInstagram, FaLocationDot } from "react-icons/fa6";
+import {
+  FaXTwitter,
+  FaFacebook,
+  FaInstagram,
+  FaLocationDot,
+  FaPhone,
+  FaEnvelope,
+} from "react-icons/fa6";
 export default function NavBar() {
   return (
-    <nav className="w-full">
+    <nav className="w-full relative">
       <section id="top-nav">
         <div>
           <p>Opening Hours: 06:00 to 20:00</p>
@@ -21,10 +28,47 @@ export default function NavBar() {
           </a>
         </div>
       </section>
-      <section>
-        <div></div>
-        <div></div>
+      <section id="middle-nav">
+        <div>
+          <a href="">
+            <p className="text-darkblue">
+              <span className="text-darkblue">BluePr</span>
+              <span className=" text-orangeyellow">intGr</span>ove
+            </p>
+          </a>
+        </div>
+        <div id="info-nav" className="flex flex-row gap-5">
+          <div className="flex flex-row">
+            <div className="rounded-full bg-orangeyellow p-4">
+              <FaEnvelope className="w-4 h-4" />
+            </div>
+            <div className="ml-2">
+              <h3>Mail Us</h3>
+              <p>info@Example.com</p>
+            </div>
+          </div>
+          <div className="flex flex-row ">
+            <div className="rounded-full bg-orangeyellow p-4">
+              <FaPhone className="w-4 h-4" />
+            </div>
+            <div className="ml-2">
+              <h3>Call Us</h3>
+              <p>+254700000000</p>
+            </div>
+          </div>
+        </div>
       </section>
+      <nav id="float-nav" className="absolute bottom-[-40px] left-[16%]">
+        <div>
+            <a href="">Home</a>
+            <a href="">About Us</a>
+            <a href="">Pages</a>
+            <a href="">Services</a>
+            <a href="">Blog</a>
+            <a href="">Contact Us</a>
+        </div>
+        <a  className="px-3 my-[-10px] bg-darkblue text-whiiteish text-center py-2" href="">Have Any Questions?</a>
+      </nav>
     </nav>
   );
 }
